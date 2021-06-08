3a186e03bd295114ea9636e47b749934cf90105c
Jesse Brink <jbrink0925@gmail.com>
2021-06-07 21:50:27 -0400

    finish assignment

--- oop.js
+++ oop.js
@@ -15,17 +15,38 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-    numwheels() {
-        return 4
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
     }
 }
 
 class Motorcycle extends Vehicle {
-    numwheels() {
-        return 2
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
     }
 
     revEngine() {
         return 'VROOM!!!';
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+
+    add(vehicle) {
+        const validVehicles = 'Car Motorcycle'.split(' ');
+        if (validVehicles.indexOf(vehicle.constructor.name) === -1) {
+            return 'Only vehicles are allowed in here!';
+        }
+        if (this.vehicles.length >= this.capacity) {
+            return 'Sorry, we\'re full.'
+        }
+        this.vehicles.push(vehicle);
+        return 'Vehicle added!';
     }
 }
